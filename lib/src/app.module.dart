161afa07f444513ed43d29c7c6ef62c3b0e2c10d
Yuri Brasil <yuri.brasil@prodepa.pa.gov.br>
2022-09-27 16:16:45 -0300

    Ajustes

--- lib/src/app.module.dart
+++ lib/src/app.module.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:modular_flutter/src/guards/auth_guards.dart';
 import 'package:modular_flutter/src/modules/auth/auth.module.dart';
